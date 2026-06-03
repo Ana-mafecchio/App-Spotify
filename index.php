b42f7c98473d18e016948b22aa76092b49175dba
ana.mf <froggitfroggers@gmail.com>
2026-06-03 19:57:30 -0300

    att

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$titulo_da_pagina = "Discos";
+include "inc-cabecalho.php"
+?>
+
+<body>
+    <main class="container">
+        <h1 class="text-center fs-2 mt-2 mb-4">Discografia</h1>
+        <div class="row">
+
+        </div>
+    </main>
+
+<?php
+include "inc-rodape.php"
+?>    
+</body>
