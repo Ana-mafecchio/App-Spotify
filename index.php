98220b2b71d390f8c47d68aff8afa169b51900c7
ana.mf <froggitfroggers@gmail.com>
2026-06-03 22:12:51 -0300

    att

--- index.php
+++ index.php
@@ -1,17 +1,41 @@
 <?php
+ 
 $titulo_da_pagina = "Discos";
-include "inc.cabecalho.php"
+include "inc-cabecalho.php";
+ 
 ?>
-
 <body>
     <main class="container">
         <h1 class="text-center fs-2 mt-2 mb-4">Discografia</h1>
-        <div class="row">
+ 
+        <div class="row mb-4">
+ 
+        <?php
+        include "inc-conexao.php";
+        $sql = "SELECT * FROM tb_discografia ORDER BY artista, ano";
+        $resultado = mysqli_query($conn, $sql);
+        while($linha = mysqli_fetch_assoc($resultado) ){
+            ?>
+            <div class="col-3">
+                <div class="cartao" style="background-image: url('<?= $linha['foto'];?>');">
+                    <div class="cor-cartao">
+                        <div class="texto-cartao p-3">
+                           <h1 class="fs-4"><?= $linha['nome'];?></h1>
 
+                            <h2 class="fs-5"><?= $linha['artista'];?></h2>
+
+                            <h3 class="fs-6"><?= $linha['tipo'];?> - <?=$linha['ano'];?></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+        mysqli_close($conn);
+        ?>
         </div>
     </main>
-
-<?php
-include "inc-rodape.php"
-?>    
 </body>
+<?php
+include "inc-rodape.php";
+?>
